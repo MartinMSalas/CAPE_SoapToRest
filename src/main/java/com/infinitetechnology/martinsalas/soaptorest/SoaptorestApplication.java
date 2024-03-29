e20c1ea46a612428d2a24fbc0e0787f57d7f8c9e
MartinMSalas <ingmartinmsalas@gmail.com>
2024-03-29 10:50:23 -0300

    adding files

---
+++ src/main/java/com/infinitetechnology/martinsalas/soaptorest/SoaptorestApplication.java
@@ -0,0 +1,13 @@
+package com.infinitetechnology.martinsalas.soaptorest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoaptorestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoaptorestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infinitetechnology/martinsalas/soaptorest/SoaptorestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infinitetechnology.martinsalas.soaptorest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoaptorestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
